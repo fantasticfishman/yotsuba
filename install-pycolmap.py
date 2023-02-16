1603a65c59e36124d429f41b263f7c43b8413d47
jadechng <chngjade@gmail.com>
2023-02-15 23:41:11 -0800

    added sus file

---
+++ install-pycolmap.py
@@ -0,0 +1,15 @@
+import pycolmap
+reconstruction = pycolmap.Reconstruction("./south-building/sparse")
+print(reconstruction.summary())
+
+for image_id, image in reconstruction.images.items():
+    print(image_id, image)
+
+for point3D_id, point3D in reconstruction.points3D.items():
+    print(point3D_id, point3D)
+
+for camera_id, camera in reconstruction.cameras.items():
+    print(camera_id, camera)
+
+reconstruction.write("./")
+reconstruction.export_PLY("sus.ply")
